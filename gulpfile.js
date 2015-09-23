f6f5ccac197028d4c8febb024107cb1352d430a4
S.Panfilov <se-panfilov@ya.ru>
2015-09-23 13:10:19 +0800

    added dev server branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,162 +1,34 @@
-'use strict';
+var gulp = require('gulp'), jade, watch, connect, minifyHTML;
 
-var gulp = require('gulp'), concat, rename, uglify, jade, sourcemaps, watch, changed,
-    ngAnnotate, stylus, nib, minifyHTML, minifyCss, templateCache, mergeStream,
-    cssBase64, size, stylish, order, jshint, install, todo;
-
-var src = {
-    styles: 'src/templates/**/*.styl',
-    jade: 'src/templates/**/*.jade',
-    js: 'src/*.js',
-    coreJs: 'bower_components/x-date-core/dist/x-date-core.js'
-};
-
-var dest = {
-    dist: 'dist',
-    src: 'src'
-};
-
-gulp.task('install', function () {
-    install = install || require("gulp-install");
-
-    gulp.src(['./bower.json', './package.json'])
-        .pipe(install());
-});
-
-gulp.task('lint', function () {
-    jshint = jshint || require('gulp-jshint');
-    stylish = stylish || require('jshint-stylish');
-
-    return gulp.src(src)
-        .pipe(jshint({
-            globalstrict: true,
-            strict: false,
-            globals: {
-                //console: true
-            }
-        }))
-        .pipe(jshint.reporter(stylish));
-});
-
-gulp.task('todo', function () {
-    todo = require('gulp-todo');
-
-    gulp.src('src/**/*.*')
-        .pipe(todo())
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('sizes', function () {
-    size = size || require('gulp-filesize');
-
-    return gulp.src([
-        'dist/**/*.js',
-        'dist/**/*.css'
-    ]).pipe(size());
-});
-
-function makeJade() {
-    templateCache = templateCache || require('gulp-angular-templatecache');
-    minifyHTML = minifyHTML || require('gulp-minify-html');
-    changed = changed || require('gulp-changed');
+gulp.task('jade', function () {
     jade = jade || require('gulp-jade');
+    minifyHTML = minifyHTML || require('gulp-minify-html');
 
-    return gulp.src(src.jade)
-        .pipe(changed(dest.dist, {extension: '.html'}))
+    return gulp.src('./*.jade')
         .pipe(jade({pretty: false}))
+        .on('error', console.log)
         .pipe(minifyHTML({
             empty: true,
             spare: true
         }))
-        .pipe(templateCache({
-            module: 'angular-pd.templates',
-            standalone: true
-        }))
-}
-
-
-function makeCoreJS() {
-    return gulp.src(src.coreJs)
-}
-
-function makeViewJS() {
-    ngAnnotate = ngAnnotate || require('gulp-ng-annotate');
-    concat = concat || require('gulp-concat');
-
-    return gulp.src(src.js)
-        .pipe(concat('view.js'))
-        .pipe(ngAnnotate({remove: true, add: true, single_quotes: true}))
-}
-
-function makeJS() {
-    mergeStream = mergeStream || require('merge-stream');
-    concat = concat || require('gulp-concat');
-    order = order || require('gulp-order');
-
-    var coreJs = makeCoreJS();
-    var viewJs = makeViewJS();
-
-    return mergeStream(coreJs, viewJs)
-        .pipe(order([
-            'x-date-core.js',
-            'view.js'
-        ]))
-        .pipe(concat('view-and-core.js'))
-        ;
-        //.pipe(gulp.dest(dest.dist));
-}
-
-function mergeJsWithTemplates(templates, mainJs) {
-    mergeStream = mergeStream || require('merge-stream');
-    sourcemaps = sourcemaps || require('gulp-sourcemaps');
-    uglify = uglify || require('gulp-uglify');
-    rename = rename || require('gulp-rename');
-    concat = concat || require('gulp-concat');
-
-    return mergeStream(templates, mainJs)
-        .pipe(concat('angular-pure-datepicker.js'))
-        .pipe(gulp.dest(dest.dist))
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename({basename: 'angular-pure-datepicker.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dest.dist));
-}
-
-gulp.task('js', function () {
-    var templates = makeJade();
-    var mainJs = makeJS();
-    return mergeJsWithTemplates(templates, mainJs);
-});
-
-gulp.task('stylus', function () {
-    cssBase64 = cssBase64 || require('gulp-css-base64');
-    minifyCss = minifyCss || require('gulp-minify-css');
-    nib = nib || require('nib');
-    stylus = stylus || require('gulp-stylus');
-    concat = concat || require('gulp-concat');
-
-    return gulp.src(src.styles, {base: 'src'})
-        .pipe(concat('angular-pure-datepicker.styl'))
-        .pipe(stylus({use: [nib()], compress: true}))
-        .pipe(cssBase64({
-            baseDir: "img"
-        }))
-        .pipe(minifyCss())
-        .pipe(gulp.dest(dest.dist));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
-    watch = watch || require('gulp-watch');
-
-    gulp.watch(src.jade, ['js', 'todo']);
-    gulp.watch(src.styles, ['stylus', 'todo']);
-    gulp.watch(src.js, ['js', 'todo']);
+    gulp.watch('./*.jade', ['jade']);
 });
 
 gulp.task('build', function () {
-    gulp.start('stylus');
-    gulp.start('js');
+    gulp.start('jade');
+});
+
+gulp.task('webserver', function () {
+    connect = connect || require('gulp-connect');
+    connect.server({
+        root: [__dirname, '../dist', 'dist'],
+        port: 8001,
+        livereload: true
+    });
 });
 
 gulp.task('default', function () {
---
+++ index.js
@@ -0,0 +1,187 @@
+'use strict';
+
+angular.module('demo', [
+    'angular-pd'
+])
+
+    .directive('modelWell', function ($filter) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                caseModel: '='
+            },
+            template: '<section class=well>' +
+            '<div><span>Day:</span>&nbsp;<span ng-bind=caseModel.model.d></span></div>' +
+            '<div><span>Month:</span>&nbsp;<span ng-bind=caseModel.model.m></span>&nbsp;<span>(+1)</span></div>' +
+            '<div><span>Year:</span>&nbsp;<span ng-bind=caseModel.model.y></span></div>' +
+            '<div><span>Datetime:</span>&nbsp;<span ng-bind=caseModel.model.dt></span>&nbsp;<span>({{getDate(caseModel.model.dt)}})</span></div>' +
+            '<div><span>Timezone:</span>&nbsp;<span ng-bind=caseModel.model.tz></span></div>' +
+            '<div><span>Start limit:</span>&nbsp;<span ng-bind=caseModel.startDate></span>&nbsp;<span>({{getDate(caseModel.startDate)}})</span></div>' +
+            '<div><span>End limit:</span>&nbsp;<span ng-bind=caseModel.endDate></span>&nbsp;<span>({{getDate(caseModel.endDate)}})</span></div>' +
+            '<div><button type="button" ng-click="plusOneMonth()">+1 month</button></div>' +
+            '<div><button type="button" ng-click="minusOneMonth()">-1 month</button></div>' +
+            '</section>',
+            link: function (scope) {
+                scope.getDate = function (datetime) {
+                    if (!datetime) return ' none ';
+
+                    return $filter('date')(new Date(datetime), 'dd-MM-yyyy');
+                };
+
+                console.dir(scope.caseModel);
+
+                scope.plusOneMonth = function () {
+                    var date = new Date(scope.caseModel.model.datetime);
+                    if (date.getMonth() === 11) {
+                        date.setMonth(0);
+                        date.setFullYear(date.getFullYear() + 1)
+                    } else {
+                        date.setMonth(date.getMonth() + 1);
+                    }
+
+                    scope.caseModel.model.datetime = date.getTime();
+                };
+
+                scope.minusOneMonth = function () {
+                    var date = new Date(scope.caseModel.model.datetime);
+                    if (date.getMonth() === 0) {
+                        date.setMonth(11);
+                        date.setFullYear(date.getFullYear() - 1)
+                    } else {
+                        date.setMonth(date.getMonth() - 1);
+                    }
+
+                    scope.caseModel.model.datetime = date.getTime();
+                };
+            }
+        };
+    })
+
+    .controller('DemoPageCtrl', function ($scope) {
+
+        $scope.localizedDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+
+        $scope.commonCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            }
+        };
+
+        $scope.limitsCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2015, 6, 21).getTime(),
+            endDate: new Date(2020, 6, 27).getTime()
+        };
+
+        $scope.leftLimitsCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2012, 4, 3).getTime()
+        };
+
+        $scope.rightLimitsCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            endDate: new Date(2015, 7, 31).getTime()
+        };
+
+        $scope.outOfRightLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            endDate: new Date(2015, 6, 21).getTime()
+        };
+
+        $scope.outOfLeftLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2015, 6, 27).getTime()
+        };
+
+        $scope.emptyModelCase = {
+            model: null
+        };
+
+        $scope.emptyModelLeftLimitCase = {
+            model: null,
+            startDate: new Date(2015, 6, 26).getTime()
+        };
+
+        $scope.emptyModelRightLimitCase = {
+            model: null,
+            endDate: new Date(2015, 6, 26).getTime()
+        };
+
+        $scope.invalidLeftLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: 'invalid'
+        };
+
+        $scope.invalidRightLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            endDate: Infinity
+        };
+
+        $scope.linkedModelCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            }
+        };
+
+        $scope.linkedModelBothLimitsCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2015, 6, 25).getTime(),
+            endDate: new Date(2015, 6, 27).getTime()
+        };
+
+        $scope.linkedModelBothDifferentLimitsCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate_one: new Date(2015, 5, 1).getTime(),
+            endDate_one: new Date(2015, 7, 31).getTime(),
+            startDate_two: new Date(2015, 6, 20).getTime(),
+            endDate_two: new Date(2015, 8, 31).getTime()
+        };
+
+        $scope.externalChangeCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            }
+        };
+
+        $scope.externalChangeLeftLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2013, 6, 25).getTime()
+        };
+
+        $scope.externalChangeRightLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            endDate: new Date(2018, 6, 27).getTime()
+        };
+
+        $scope.externalChangeBothLimitCase = {
+            model: {
+                datetime: new Date(2015, 6, 26).getTime()
+            },
+            startDate: new Date(2013, 6, 25).getTime(),
+            endDate: new Date(2018, 6, 27).getTime()
+        };
+    })
+;
